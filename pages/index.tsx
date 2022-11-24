77475d072502dfb1d8643f5faf09f2cf9409f4d1
Herman <smurovnemez@mail.ru>
2022-11-24 11:33:00 -0600

    feat: project settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home(): JSX.Element {
+  const a = 1;
   return (
     <div className={styles.container}>
       <Head>
@@ -60,12 +61,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{a}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer>                                       
     </div>
-  )
+  );
 }
