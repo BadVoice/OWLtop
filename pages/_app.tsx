4a41f77dbd2a8d24cc8b48515299b4e94a3eba5a
Herman <smurovnemez@mail.ru>
2022-11-28 15:14:06 -0600

    feat: colors & fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,9 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
     <Head>
         <title>OWLtop</title>
         <link  rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous'  />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
     </Head>
     <Component {...pageProps} />;
   </>  // <> </> => React.Fragment
