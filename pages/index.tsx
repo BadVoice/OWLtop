f24d6d92af406d50a35cc5c8ce5cc1b76334e59c
Herman <smurovnemez@mail.ru>
2022-12-01 12:53:46 -0600

    feat: add Button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 
 import Image from 'next/image';
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 
 export default function Home(): JSX.Element {
  
   return (
     <>
-      <Htag tag='h1'>Text</Htag>               
+      <Htag tag='h1'>Text</Htag>
+      <Button appearance='primary'> Кнопка</Button>
+      <Button appearance='ghost'> Кнопка</Button>
     </>
   );
 }
