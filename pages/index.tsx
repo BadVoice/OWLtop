0cb803b0f2ede1f51ee1b3f454b5181f870bafa9
Herman <smurovnemez@mail.ru>
2022-12-02 08:27:25 -0600

    feat: update DetailedHTMLProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Text</Htag>
-      <Button appearance='primary'> Кнопка</Button>
+      <Button appearance='primary' className='dfhfd'> Кнопка</Button>
       <Button appearance='ghost'> Кнопка</Button>
     </>
   );
